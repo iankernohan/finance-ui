8ef1e67ee4831131bafd8686c93a1c7e14b8a293
Ian Kernohan <kernohani1@gmail.com>
2026-03-13 22:01:17 -0400

    no retries

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
 
 function App() {
   return (
